f70c637b57f692e2ac93428011838da556901bd0
sagar7250 <hemasagar27521@gmail.com>
2024-09-26 20:49:24 +0530

    Basics JavaScript Prolems

---
+++ index.js
@@ -0,0 +1,10 @@
+// Iam considering you will be aware of Chrome Dev tools,
+//If Not Kindly Check in youtube
+
+
+/*If You Want to print the hello world in console in JavaScript*/
+//console.log("Hello World!")
+
+
+
+
